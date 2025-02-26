70beb5196c9de85cee904417d51f503a88e59166
alpapaydin <alpapaydin@windowslive.com>
2025-02-27 02:38:07 +0300

    add sounds

--- Assets/EmojiMerge/Scripts/Grid/Items/Container/ContainerItemDefinition.cs
+++ Assets/EmojiMerge/Scripts/Grid/Items/Container/ContainerItemDefinition.cs
@@ -6,5 +6,6 @@ using UnityEngine;
 public class ContainerItemDefinition : BaseItemProperties
 {
     // Item property definition which chest and producer will inherit from. move inventory and recharge related properties here.
-    // make another struct ContainerItemLevel, which will hold contained items and recharge times for each level of item.
-}
+    // make another struct ContainerItemLevel, which will hold contained item type (base item definition), item level, item count and recharge times for each level of item.
+    // also refactor resource items to inherit from produced items
+}   
